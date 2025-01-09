4084b70a857aee21203da35bbfe5285fbd8968f7
32-Sinha <unacadmya@gmail.com>
2025-01-09 19:07:29 +0600

    testing project

--- Splash.java
+++ Splash.java
@@ -1,8 +1,43 @@
 
 import javax.swing.*;
-public class Splash extends JFrame {
-    Splash(){ setSize(1200,600);
+import java.awt.*;
+
+
+public class Splash extends JFrame implements Runnable {
+    Thread thread;
+    Splash()
+    { //setSize(1200,600);
+        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icons/C:\\Users\\USER\\Downloads\\OIP.jpg"));
+        Image i2 = i1.getImage().getScaledInstance(1200,600, Image.SCALE_DEFAULT);
+        ImageIcon i3= new ImageIcon(i2);
+        JLabel image = new JLabel(i3);
+        add(image);
+
         setVisible(true);
+        thread= new Thread(this);
+        thread.start();
 }
-public static void main(String[]args){ new Splash();}
-}
+public void run() {
+    try {
+        Thread.sleep(7000);
+        setVisible(false);
+
+    } catch (Exception e) {
+
+    }}
+        public static void main (String[]args)
+        {
+            Splash frame = new Splash();
+            int x = 1;
+            for(int i = 1; i <= 500; x += 7, i += 6)
+            {
+                frame.setLocation(750 - (x + i) / 2, 400 - (i / 2));
+                frame.setSize(x + i, i);
+                try{
+                Thread.sleep(10);} catch (Exception e ){}
+
+            }
+
+
+        }
+    }
--- vid02.java
+++
@@ -1,2 +0,0 @@
-public class vid02 {
-}
